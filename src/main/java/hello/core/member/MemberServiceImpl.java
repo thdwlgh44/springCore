2b8455e1c423ade0a165021c4fb7f6d19c0e2f95
jiho <thdwlgh44@naver.com>
2022-09-23 15:57:26 +0900

    spring core project: day0923 component scan

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,16 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+//빈 이름: 클래스명
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired //ac.getBean(MemberRepository.class)
+    //생성자에 파라미터가 많아도 다 찾아서 자동으로 주입한다.
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
